767951140b8fa9da586948cde7702a51fa7c9c4e
zyg2020 <zhuyange2018@gmail.com>
2021-11-08 12:12:40 -0600

    Scaffold Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+csv_file = Rails.root.join("db/dataset_excel.csv")
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true, encoding: "iso-8859-1")
+
+# If CSV was created by Excel in Windows you may also need to set an encoding type:
+# products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |p|
+  print p
+end
+
+Pages
+Category
+Province
+Customer
+Status
+Order
+Product
+OrderProduct
+ProductCategory
