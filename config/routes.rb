c2adeb43d5efd26892421064ed65949ac655e408
zyg2020 <zhuyange2018@gmail.com>
2021-11-08 15:36:56 -0600

    Add Active Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :order_products
   resources :product_categories
   resources :products
@@ -8,5 +9,7 @@ Rails.application.routes.draw do
   resources :provinces
   resources :categories
   resources :pages
+
+  root to: "products#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
