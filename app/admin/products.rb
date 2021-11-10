41e941e103a92bb88785617e157db4ab24414c1e
zyg2020 <zhuyange2018@gmail.com>
2021-11-10 01:34:08 -0600

    Add image field to Product

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,11 +1,10 @@
 ActiveAdmin.register Product do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :description, :price, :previous_price
+  permit_params :name, :description, :price, :previous_price, :image
   #
   # or
   #
@@ -15,4 +14,13 @@ ActiveAdmin.register Product do
   #   permitted
   # end
 
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    # let's add this piece:
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions # adds the 'Submit' and 'Cancel' buttons
+  end
 end
